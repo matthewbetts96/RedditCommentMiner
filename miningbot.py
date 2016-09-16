3c40c48c88be3934a2795bceaa125d8ffbbff020
Matthew Betts <matthew.betts.96@gmail.com>
2016-09-16 17:40:09 +0100

    removed more stuff

--- miningbot.py
+++ miningbot.py
@@ -5,7 +5,6 @@ app_uri = 'https://127.0.0.1:65010/authorize_callback'
 app_scopes = 'account creddits edit flair history identity livemanage modconfig modcontributors modflair modlog modothers modposts modself modwiki mysubreddits privatemessages read report save submit subscribe vote wikiedit wikiread'
 app_refresh = ''
 
-
 import praw
 import time
 import sqlite3
@@ -18,18 +17,14 @@ cur = sql.cursor()
 print("Creating database if it doesn't exist")
 cur.execute('CREATE TABLE IF NOT EXISTS posts (COMMENTS varchar NOT NULL)')
 sql.commit
- 
+
 print("Accessing account...")
 r = praw.Reddit(app_ua)
 r.set_oauth_app_info(app_id, app_secret, app_uri)
 r.refresh_access_information(app_refresh)
 print('Logged in.')
-
 while (True):
-    print("Fetching subreddit " + SUBREDDIT)
-    #subreddit = r.get_subreddit(SUBREDDIT)
-    print("Fetching Comments...")
-    #all_comments = subreddit.get_comments()
+    print("Fetching subreddit " + SUBREDDIT + "and comments.")
     print("Adding comments...")
     for comment in praw.helpers.comment_stream(r, SUBREDDIT):
         text = comment.body
